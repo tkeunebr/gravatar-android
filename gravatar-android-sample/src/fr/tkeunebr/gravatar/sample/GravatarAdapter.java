8e4d8f0da7522a62c933a63ec0c8d824faf8a6e8
Thomas <thomas.keunebroek@gmail.com>
2013-05-25 16:10:38 +0200

    Reindent the Android sample app as well

--- gravatar-android-sample/src/fr/tkeunebr/gravatar/sample/GravatarAdapter.java
+++ gravatar-android-sample/src/fr/tkeunebr/gravatar/sample/GravatarAdapter.java
@@ -14,54 +14,54 @@ import java.util.Collections;
 import java.util.List;
 
 final class GravatarAdapter extends BaseAdapter {
-	private final Context mContext;
-	private final LayoutInflater mLayoutInflater;
-	private final int mAvatarImageViewPixelSize;
-	private List<GravatarActivity.User> mUsers = Collections.emptyList();
+  private final Context mContext;
+  private final LayoutInflater mLayoutInflater;
+  private final int mAvatarImageViewPixelSize;
+  private List<GravatarActivity.User> mUsers = Collections.emptyList();
 
-	public GravatarAdapter(Context context) {
-		mContext = context;
-		mLayoutInflater = LayoutInflater.from(context);
-		mAvatarImageViewPixelSize = context.getResources().getDimensionPixelSize(R.dimen.avatar_image_view_size);
-	}
+  public GravatarAdapter(Context context) {
+    mContext = context;
+    mLayoutInflater = LayoutInflater.from(context);
+    mAvatarImageViewPixelSize = context.getResources().getDimensionPixelSize(R.dimen.avatar_image_view_size);
+  }
 
-	public void updateUsers(List<GravatarActivity.User> users) {
-		mUsers = users;
-		notifyDataSetChanged();
-	}
+  public void updateUsers(List<GravatarActivity.User> users) {
+    mUsers = users;
+    notifyDataSetChanged();
+  }
 
-	@Override
-	public int getCount() {
-		return mUsers.size();
-	}
+  @Override
+  public int getCount() {
+    return mUsers.size();
+  }
 
-	@Override
-	public GravatarActivity.User getItem(int position) {
-		return mUsers.get(position);
-	}
+  @Override
+  public GravatarActivity.User getItem(int position) {
+    return mUsers.get(position);
+  }
 
-	@Override
-	public long getItemId(int position) {
-		return position;
-	}
+  @Override
+  public long getItemId(int position) {
+    return position;
+  }
 
-	@Override
-	public View getView(int position, View convertView, ViewGroup parent) {
-		if (convertView == null) {
-			convertView = mLayoutInflater.inflate(R.layout.user_list_item, null);
-		}
+  @Override
+  public View getView(int position, View convertView, ViewGroup parent) {
+    if (convertView == null) {
+      convertView = mLayoutInflater.inflate(R.layout.user_list_item, null);
+    }
 
-		GravatarActivity.User user = getItem(position);
+    GravatarActivity.User user = getItem(position);
 
-		String gravatarUrl = Gravatar.init().with(user.email).force404().size(mAvatarImageViewPixelSize).build();
-		Picasso.with(mContext)
-				.load(gravatarUrl)
-				.placeholder(R.drawable.ic_contact_picture)
-				.error(R.drawable.ic_contact_picture)
-				.into((ImageView) convertView.findViewById(R.id.user_avatar));
+    String gravatarUrl = Gravatar.init().with(user.email).force404().size(mAvatarImageViewPixelSize).build();
+    Picasso.with(mContext)
+            .load(gravatarUrl)
+            .placeholder(R.drawable.ic_contact_picture)
+            .error(R.drawable.ic_contact_picture)
+            .into((ImageView) convertView.findViewById(R.id.user_avatar));
 
-		((TextView) convertView.findViewById(R.id.user_name)).setText(user.name);
+    ((TextView) convertView.findViewById(R.id.user_name)).setText(user.name);
 
-		return convertView;
-	}
+    return convertView;
+  }
 }
